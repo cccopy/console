3f08c054bcaa3c5473ad812d2a6ddb236e66263e
KinoLien <miecowbai@gmail.com>
2019-05-13 14:09:11 +0800

    Make tags selection can be autocrat.

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -3,10 +3,20 @@
 // Select2
 $(function() {
 	$('.select2-tags').each(function() {
-		$(this).wrap('<div class="position-relative"></div>')
+		var self = $(this);
+		self.wrap('<div class="position-relative"></div>')
 			.select2({
+				tags: self.attr("autoCreate") != undefined,
 				placeholder: '請點選',
-				dropdownParent: $(this).parent()
+				dropdownParent: self.parent()
+			})
+			.on("select2:select", function(evt){
+				var element = evt.params.data.element;
+				var $element = $(element);
+
+				$element.detach();
+				$(this).append($element);
+				$(this).trigger("change");
 			});
-	})
+	});
 });
